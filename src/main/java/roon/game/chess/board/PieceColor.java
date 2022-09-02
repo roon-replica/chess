b5c6f091218069424dc473299c3e386f05be5c82
roon-replica <think-roon@naver.com>
2022-09-02 17:26:42 +0900

    #2 보드, 플레이어

---
+++ src/main/java/roon/game/chess/board/PieceColor.java
@@ -0,0 +1,12 @@
+package roon.game.chess.board;
+
+public enum PieceColor {
+    WHITE("white"), BLACK("black");
+
+    private String color;
+
+    PieceColor(String color){
+        this.color = color;
+    }
+
+}
