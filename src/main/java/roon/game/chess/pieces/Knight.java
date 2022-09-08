09a5ef2685fdb224d53f260eb6bd0dd94a4e7fcb
roon-replica <think-roon@naver.com>
2022-09-08 22:22:54 +0900

    기물 이동 구현 - knight

--- src/main/java/roon/game/chess/pieces/Knight.java
+++ src/main/java/roon/game/chess/pieces/Knight.java
@@ -1,9 +1,10 @@
 package roon.game.chess.pieces;
 
 import lombok.Builder;
+import roon.game.chess.board.Board;
 import roon.game.chess.board.PieceColor;
 
-public class Knight extends AbstractPiece{
+public class Knight extends AbstractPiece {
 
     @Builder
     public Knight(PieceColor pieceColor, char file, char rank) {
@@ -15,8 +16,41 @@ public class Knight extends AbstractPiece{
     }
 
     @Override
-    public String toString(){
-        var ret = pieceColor.name() +" "+ this.symbol.name();
+    public String toString() {
+        var ret = pieceColor.name() + " " + this.symbol.name();
         return decoratePrint(ret);
+    }
+
+    @Override
+    public void move(char nextFile, char nextRank, Board board) {
+        boolean canmove = this.validate(nextFile, nextRank, board); //todo:  this필요?
+        if (canmove == false) {
+            System.out.println("can not move");
+            return;
+        }
+
+        this.file = nextFile;
+        this.rank = nextRank;
+    }
+
+    private static int[][] canMoves = new int[][]{{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}};
+
+    @Override
+    protected boolean validate(char nextFile, char nextRank, Board board) {
+        if (board.isEmpty(nextFile, nextRank) == false) {
+            return false;
+        }
+
+        for (var canMove : canMoves) {
+            char file = (char) (this.file + canMove[0]);
+            char rank = (char) (this.rank + canMove[1]);
+
+            if (file == nextFile && rank == nextRank) {
+                return true;
+            }
+        }
+
+        return false;
+
     }
 }
